07c57fe9aabdafe833f173661629456bd4e38807
Michal Mizera <axadiw@gmail.com>
2015-06-10 15:57:02 +0200

    Updated basset_ios to 1.1

--- basset_ios.rb
+++ basset_ios.rb
@@ -1,51 +1,50 @@
 class BassetIos < Formula
+  desc "Converts vector images to PNG and puts them in xcassets"
+  homepage "https://github.com/Polidea/basset-ios"
+  url "https://github.com/Polidea/basset-ios/archive/1.1.tar.gz"
+  sha256 "6ee658f4022fc4d29acc0bc0dbc94e5468fd88e2cc3cdde6ae7a78b9240c8e40"
 
-    desc "Converting vector images to PNG(s) and organizes them in xcassets"
-    homepage "https://github.com/Polidea/basset-ios"
-    url "https://github.com/Polidea/basset-ios/archive/1.0.tar.gz"
-    sha1 "18043152e4642e5baa5aad1c8bd2dda2546edefd"
+  depends_on :python if MacOS.version <= :snow_leopard
+  depends_on "imagemagick"
+  depends_on "ghostscript"
 
-    depends_on :python if MacOS.version <= :snow_leopard
-    depends_on "imagemagick" => :build_from_source
-    depends_on "ghostscript"
+  resource "coloredlogs" do
+    url "https://pypi.python.org/packages/source/c/coloredlogs/coloredlogs-1.0.tar.gz"
+    sha256 "35e1e8a422b27d8c8e66eaa30d5dfff80d9c233bd52543e10b79688781b0510b"
+  end
 
-    resource "coloredlogs" do
-        url "https://pypi.python.org/packages/source/c/coloredlogs/coloredlogs-1.0.tar.gz"
-        sha1 "3ee63fac5640c9c8185814634f32656f837ee90d"
+  resource "Wand" do
+    url "https://pypi.python.org/packages/source/W/Wand/Wand-0.4.0.tar.gz"
+    sha256 "3ef8cbadb122808c123b33d34a9cc833a91eebb700ebea1d5d610fa807f55a89"
+  end
+
+  resource "PyYAML" do
+    url "https://pypi.python.org/packages/source/P/PyYAML/PyYAML-3.11.tar.gz"
+    sha256 "c36c938a872e5ff494938b33b14aaa156cb439ec67548fcab3535bb78b0846e8"
+  end
+
+  resource "humanfriendly" do
+    url "https://pypi.python.org/packages/source/h/humanfriendly/humanfriendly-1.26.tar.gz"
+    sha256 "0908b90298364e3335a03abcbb88ec571f95c7e4bc6728de0551806f636ef219"
+  end
+
+  def install
+    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
+    resources.each do |r|
+      r.stage do
+        system "python", *Language::Python.setup_install_args(libexec/"vendor")
+      end
     end
 
-    resource "Wand" do
-        url "https://pypi.python.org/packages/source/W/Wand/Wand-0.4.0.tar.gz"
-        sha1 "672c286e5202501f228145362db66a9a866b30d1"
-    end
+    ENV.prepend_create_path "PYTHONPATH", libexec
 
-    resource "PyYAML" do
-        url "https://pypi.python.org/packages/source/P/PyYAML/PyYAML-3.11.tar.gz"
-        sha1 "1a2d5df8b31124573efb9598ec6d54767f3c4cd4"
-    end
+    libexec.install "basset"
+    bin.install "basset_ios"
 
-    resource "humanfriendly" do
-        url "https://pypi.python.org/packages/source/h/humanfriendly/humanfriendly-1.26.tar.gz"
-        sha1 "90bb0dec1be538f8b07764c15079eddc4d5dcfb6"
-    end
+    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+  end
 
-    def install
-        ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
-        resources.each do |r|
-          r.stage do
-            system "python", *Language::Python.setup_install_args(libexec/"vendor")
-          end
-        end
-
-        ENV.prepend_create_path "PYTHONPATH", libexec
-
-        libexec.install Dir["basset"]
-        bin.install "basset_ios"
-
-        bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
-    end
-
-    test do
-        system "#{bin}/basset_ios", "-h"
-    end
+  test do
+    system "#{bin}/basset_ios", "-h"
+  end
 end
